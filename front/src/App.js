dd52d39160027a7ec42ba3b39602215a935fa69f
johao70 <perlazajohao@gmail.com>
2020-02-21 15:26:49 -0500

    changes in the presentation of the cards

--- front/src/App.js
+++ front/src/App.js
@@ -52,9 +52,42 @@ export default class App extends Component {
           </div>
           <div className="bg-white" >
             {
-              this.state.cartasUser.map(item => 
-                <div className="bg-black">
-                  <h2 className="text-white">{`${item.rotulo} ${item.palo}`}</h2>
+              this.state.cartasUser.map( element =>
+                <div className="w-64 cursor-pointer border b-gray-400 rounded flex flex-col justify-center items-center text-center p-6 bg-white">
+                  <div className="text-md font-bold text-gray-900 flex justify-between w-full">
+                    <span className="uppercase">{ element.rotulo }</span>
+                  </div>
+                  <div className="text-md font-bold text-gray-900 flex justify-between w-full">
+                    <span className="uppercase">{ element.palo.nombre }</span>
+                  </div>
+                  
+                  <div className="w-32 h-32 flex items-center justify-center">
+      
+                  </div>
+      
+                  <p className="text-gray-700 mb-2">{ element.imagen }</p>
+                  
+                  <div className="text-3xl font-bold text-gray-900 mb-6">
+                    
+                  </div>
+      
+                  <div className="text-md font-bold text-gray-900 flex justify-between w-full">
+                    <div className="flex items-center text-gray-700 px-2">
+                      
+                    </div>
+                    <div className="flex items-center text-gray-700 px-2">
+                      <span className="uppercase">{ element.palo.nombre }</span>
+                    </div>
+                  </div>
+      
+                  <div className="text-md font-bold text-gray-900 flex justify-between w-full">
+                    <div className="flex items-center text-gray-700 px-2">
+                      
+                    </div>
+                    <div className="flex items-center text-gray-700 px-2">
+                      <span className="uppercase">{ element.rotulo }</span>
+                    </div>
+                  </div>
                 </div>
               )
             }
